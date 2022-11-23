506e0b995e221322b406dcbbba4f4204b7a54c44
Nataliia Bielanova <frojlan@gmail.com>
2022-11-23 07:06:46 +0200

    Updates

---
+++ index.b5f26095.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button"),o=document.querySelector("body");let t=["darkslateblue","midnightblue","teal","tomato","seagreen","royalblue","saddlebrown","indigo","olivedrab","rosybrown","coral","moccasin","darkviolet","purple","aquamarine","turquoise","chocolate","plum"];e.addEventListener("click",(function(){const e=Math.floor(Math.random()*t.length);o.style.setProperty("--bg",t[e])}));
+//# sourceMappingURL=index.b5f26095.js.map
