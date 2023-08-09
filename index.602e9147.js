6240fd41dc7889871d30d803068d34fd0a558d8f
Nataleesha <frojlan@gmail.com>
2023-08-10 00:05:15 +0300

    Updates

---
+++ index.602e9147.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button"),o=document.querySelector("body");let t=["darkslateblue","midnightblue","teal","tomato","seagreen","royalblue","saddlebrown","indigo","olivedrab","rosybrown","coral","moccasin","darkviolet","purple","aquamarine","turquoise","chocolate","plum"];e.addEventListener("click",function(){let e=Math.floor(Math.random()*t.length);o.style.setProperty("--bg",t[e])});
+//# sourceMappingURL=index.602e9147.js.map
